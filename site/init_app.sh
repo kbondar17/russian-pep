61d0b83231ea57b91fbcc7ebb1f2e49d79889b12
кирилл бондарь <ironbondar@gmail.com>
2022-10-06 16:30:24 +0300

    script fix

--- site/init_app.sh
+++ site/init_app.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-uwsgi --ini app/uwsgi.ini > logs/uwsgi_logs &
-#python -m celery -A app worker --concurrency=1 -s /home/celery/var/run/celerybeat-schedule --loglevel=info -B &> logs/celery_beats.logs &
+uwsgi --ini app/uwsgi.ini 
