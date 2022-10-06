646cfc7658be7398021e3228fd9f02a981146d1c
кирилл бондарь <ironbondar@gmail.com>
2022-10-06 12:14:14 +0300

    init fix

--- site/init_db.sh
+++ site/init_db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 python manage.py migrate
-python manage.py collectstatic -y
+python manage.py collectstatic --noinput
 python manage.py createsuperuser --no-input --username=kirill  --email=joe@example.com
 python -m scripts.load
-uwsgi --ini app/uwsgi.ini --attach-daemon2 'cmd=celery -A app worker --concurrency=1 -s /home/celery/var/run/celerybeat-schedule --loglevel=info -B &> logs/celery_beats.logs'
 
+uwsgi --ini app/uwsgi.ini > logs/uwsgi_logs &
+celery -A app worker --concurrency=1 -s /home/celery/var/run/celerybeat-schedule --loglevel=info -B &> logs/celery_beats.logs &
